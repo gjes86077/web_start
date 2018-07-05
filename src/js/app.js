60b35758e7bd3ea10aa6dc8c70e668c08fe242f6
Turking <gjes86077@gmail.com>
2018-07-06 01:31:26 +0800

    Webpack SCSS BABEL BOOTSTRAP

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import { BODY } from './init';
+import '../scss/_var.scss';
+import '../scss/all.scss';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../../node_modules/bootstrap/scss/bootstrap.scss'
+$(function () {
+  $(".btn").click(() => {
+    alert();
+  })
+})
